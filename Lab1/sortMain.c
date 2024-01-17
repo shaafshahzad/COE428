27073264fcae0c298c470dcc421052660c8ba1f8
Shaaf <shaaf.m.shahzad@gmail.com>
2024-01-17 15:42:49 -0500

    Updated data values

--- Lab1/sortMain.c
+++ Lab1/sortMain.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include "mySort.h"
 
-int main(int argc, char * argv[]){
+int main(int argc, char *argv[])
+{
 
     int data[100000]; /* Array of ints to sort */
     int nDataItems;   /* number of actual items in the array */
@@ -17,22 +18,23 @@ int main(int argc, char * argv[]){
 
     mySort(data, nDataItems);
 
-
     /* Check that the data array is sorted. */
-    for(i = 0; i < nDataItems-1; i++) {
-        if (data[i] > data[i+1]) {
+    for (i = 0; i < nDataItems - 1; i++)
+    {
+        if (data[i] > data[i + 1])
+        {
             fprintf(stderr, "Sort error: data[%d] (= %d)"
-              " should be <= data[%d] (= %d)- -aborting\n",
-		    i, data[i], i+1, data[i+1]);
+                            " should be <= data[%d] (= %d)- -aborting\n",
+                    i, data[i], i + 1, data[i + 1]);
             exit(1);
-	}
+        }
     }
 
     /* Print sorted array to stdout */
-    for(i = 0; i < nDataItems; i++) {
+    for (i = 0; i < nDataItems; i++)
+    {
         printf("%d\n", data[i]);
     }
 
     exit(0);
-    
 }
