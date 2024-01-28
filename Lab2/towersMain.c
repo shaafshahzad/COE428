05f65720ae6cc7971a74a6716f1e77d562e50c0b
Shaaf <shaaf.m.shahzad@gmail.com>
2024-01-28 05:47:49 -0500

    Completed lab 2

--- Lab2/towersMain.c
+++ Lab2/towersMain.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "towers.h"
 
 int main(int argc, char **argv)
@@ -6,10 +7,23 @@ int main(int argc, char **argv)
     int n = 3;
     int from = 1;
     int dest = 2;
-    if (argc > 1) {
+    if (argc == 2)
+    {
         n = atoi(argv[1]);
     }
+    else if (argc == 4)
+    {
+        n = atoi(argv[1]);
+        from = atoi(argv[2]);
+        dest = atoi(argv[3]);
+
+        if (from == dest)
+        {
+            fprintf(stderr, "Error: from and dest cannot be the same");
+            exit(0);
+        }
+    }
+
     towers(n, from, dest);
     exit(0);
 }
-
